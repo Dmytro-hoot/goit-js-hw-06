38a48262a2fffb0249fbdb1d09e06eb855e11e2b
Dmytro Hoot <46649352+Dmytro-hoot@users.noreply.github.com>
2023-07-22 19:50:28 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChange = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorName = document.querySelector('span.color');
+
+buttonChange.addEventListener('click', e => {
+	const randomColor = getRandomHexColor();
+	body.style.backgroundColor = randomColor;
+	colorName.textContent = randomColor;
+
+})
